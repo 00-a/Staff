9f228542cfdb8860abf785defeba8272af6c4601
00-a <qwertqwerty70@gmail.com>
2021-05-17 18:04:14 +0500

    Add JWT token

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,19 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('staff.urls')),
-    path('api/', include('rest_framework.urls'))
+    path('api/', include('rest_framework.urls')),
+    path('api/views/', include('staff.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view())
 ]
 
 if settings.DEBUG:
